33a62ffb2d89993cb5e273e195a47fcb55241b64
Tainan <tainan206@gmail.com>
2021-11-22 17:04:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-validation';
+export class AppComponent implements OnInit {
+
+  // @ts-ignore
+  formulario: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.formulario = this.formbuilder.group({
+      nome: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(30)]],
+      sobrenome: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(30)]],
+      email: [null, [Validators.required, Validators.email]]
+    });
+  }
+
+  saveData(): void {
+    alert('Olha a gente aqui!!! \n\nSubmitou!!!');
+    this.resetForm();
+  }
+
+
+  resetForm(): void {
+    this.formulario.reset();
+  }
 }
